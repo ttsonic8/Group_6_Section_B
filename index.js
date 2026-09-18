5106fba0a44af4ee3b6bbd93492b633976959b67
ttsonic8 <chatorres143@gmail.com>
2026-09-18 10:04:32 +0800

    docs: Midterm Activity

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express')
 
 const app = express()
 
+express.json()
+
+let data = [{
+    id: 1,
+    name: "Dog",
+}]
+
 app.use('/', require('./routes/users.routes'))
 app.use('/api/users', require('./routes/users.routes'))
 app.use('/api/me', require('./routes/users.routes'))
+
+app.get('/api/users', (res,req) => {
+    return res.json({
+        ...data
+    })
+})
+
+app.get('/api/products')
 
 app.listen(1234, () => {
 	console.log('Server is running on http://localhost:1234')
